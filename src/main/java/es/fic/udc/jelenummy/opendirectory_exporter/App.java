2a6323e9f5c54c0ea25604c6eb23f1f7fe44b04c
malvarezcastillo <m.castillo@udc.es>
2017-10-02 11:45:34 +0200

    Start maven project

---
+++ src/main/java/es/fic/udc/jelenummy/opendirectory_exporter/App.java
@@ -0,0 +1,13 @@
+package es.fic.udc.jelenummy.opendirectory_exporter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
