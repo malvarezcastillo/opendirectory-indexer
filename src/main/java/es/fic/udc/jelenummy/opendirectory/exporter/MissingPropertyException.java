f3421af5a32ddc3d517ecd1b588f60dfa20fa6d3
Martín Álvarez Castillo <matrixrevoluxions@gmail.com>
2017-10-13 19:38:33 +0200

    Add Exception File

---
+++ src/main/java/es/fic/udc/jelenummy/opendirectory/exporter/MissingPropertyException.java
@@ -0,0 +1,10 @@
+package es.fic.udc.jelenummy.opendirectory.exporter;
+
+public class MissingPropertyException extends Exception {
+
+	private static final long serialVersionUID = 5289964861712029504L;
+
+	public MissingPropertyException(String message) {
+		super(message);
+	}
+}
